09353b781291b2b24c62c85088bb197bf0b86336
Bedrock02 <JimSteve91@gmal.com>
2017-05-27 00:16:38 -0700

    initial material

---
+++ homework/homework_lecture_1.py
@@ -0,0 +1,103 @@
+# Find out the value of z or other requested variables and send your results to
+# JimSteve91@gmail.com
+# For a python interpreter or to use python
+# 1. Use Mac's terminal
+# 2. https://repl.it/languages/python3
+
+# Note: The idea of these exercises are for you to practice running python
+# and writting python code. Let the code do the math for you
+
+# You can always run your code in the python interpeter
+
+#################
+###### SUM ######
+#################
+
+x = 879879879879879879879
+y = 234234234234234234234
+
+
+# Q1. What does Z equal to?
+z = x + y
+
+# Q2. What does Z equal to now?
+z += 100100100100100100100
+
+# Q3. What does z equal to
+z = 0
+z += 1
+
+#########################
+###### SUBTRACTION ######
+#########################
+# Q4. what does Z equal to after this new equation
+z = 502550525505 - 25505050
+
+# Q5. What does Z equal to now
+z -= 28394
+
+
+############################
+###### Multiplication ######
+############################
+# Q6. What does Z equal after the product of these two
+a = 19911991
+z = a * 2
+
+######################
+###### Division ######
+######################
+# Q7. What does Z equal after the product of these two
+a = 20000000000000
+z = a / 2
+
+# Q8 What does z evaluate to now?
+z = a / 100000
+
+########################
+###### Remainders ######
+########################
+# Q9. What does z evaluate to? What does the remainder evaluate to?
+z = 6 / 5
+remainder = 6 % 5
+
+# Q10. What does z evaluate to? What does the remainder evaluate to?
+z = 6 / 2
+remainder = 6 % 2
+
+
+########################
+###### Remainders ######
+########################
+# Q11. What does z evaluate to?
+z = -3
+
+# Q12. What does z evaluate to?
+z = -z
+
+########################
+###### Absolutes  ######
+########################
+# Q13. What does z evaluate to?
+z = abs(-123852987582345)
+
+####################
+###### POWER  ######
+####################
+# Q13. What does z evaluate to?
+z = 2 ** 2
+
+####################
+###### Combo  ######
+####################
+# Q14. What does z evaluate to?
+z = 2 + 3 * 4
+
+# Q15. What does z evaluate to?
+z = (2 + 3) * 4
+
+# Q16. What does z evaluate to?
+z = 2 * (4 - 1)
+
+# Q17. What does z evaluate to?
+z = -(2 + 3)
---
+++ homework/homework_lecture_2.py
@@ -0,0 +1,49 @@
+# Find out the value of z or other requested variables and send your results to
+# JimSteve91@gmail.com
+# For a python interpreter or to use python
+# 1. Use Mac's terminal
+# 2. https://repl.it/languages/python3
+
+
+text = """
+I said a hip hop
+Hippie to the hippie
+The hip, hip a hop, and you don't stop, a rock it out
+Bubba to the bang bang boogie, boobie to the boogie
+To the rhythm of the boogie the beat
+
+Now, what you hear is not a test I'm rappin' to the beat
+And me, the groove, and my friends are gonna try to move your feet
+See, I am Wonder Mike, and I'd like to say hello
+To the black, to the white, the red and the brown
+The purple and yellow, but first, I gotta
+
+Bang bang, the boogie to the boogie
+Say up jump the boogie to the bang bang boogie
+Let's rock, you don't stop
+Rock the rhythm that'll make your body rock
+Well so far you've heard my voice but I brought two friends along
+And the next on the mic is my man Hank
+C'mon, Hank, sing that song, check it out
+"""
+
+
+# Q1. How many times does the word boogie occur in the text?
+
+# Q2. How many ' ' (blank) characters is this string of text?
+
+# Q3. How many characters is this string of text?
+
+# Q4. Does this string of text end with \n?
+
+# Q5. What happens when you use the split string method? ex. str.split()
+
+# Q6. What happens when you split the string over a comma? ex. str.split(',')
+
+# Q7. What happens when you split the string over a newline character ex str.split('\n')
+
+# Q8. What index can I find the first instance of the word 'Wonder'. Hint: use the 'find' method
+
+# Q9. Replace every word 'boogie' with the word 'woogie'. Hint: use replace
+
+# Q10. Print (copy the output) you get when you join each character with a space. Hint: use 'join'
